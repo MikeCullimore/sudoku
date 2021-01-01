3d855bf7a53a500c0b33f79430f756bed85fae0b
Mike Cullimore <michael.cullimore11@gmail.com>
2021-01-01 19:49:22 +0000

    Added Cell class.

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,13 @@
 """
 sudoku.py
+
+todo:
+How to implement rules?
+Web visualisation (React? Elm? Vanilla JS?).
 """
 
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Example from card.
 example = [
@@ -16,7 +22,6 @@ example = [
     [1, None, None, None, None, None, 9, 8, None]
 ]
 
-
 # Example puzzle used by Dad.
 example2 = [
     [None, None, 7, None, None, None, None, None, None],
@@ -30,9 +35,30 @@ example2 = [
     [None, 1, None, 9, 7, None, None, 6, None]
 ]
 
+
+class Cell:
+    def __init__(self, value=None):
+        if value is not None:
+            self._value = value
+            self._options = [value]
+        else:
+            self._value = value
+            self._options = digits
+
+
 class Sudoku:
+    """
+    Refactor using Cell class.
+        1d list? Helper functions to convert given column, row and block.
+    Getters for columns, rows and blocks. Also for a given number? ("Where are all the 1s?")
+    Initialise with numbers and their positions (no None)?
+    """
+
     def __init__(self, cells=example2):
         self._cells = cells
+    
+    def solve(self):
+        pass
     
     def print(self):
         for row in self._cells:
@@ -42,9 +68,6 @@ class Sudoku:
                     continue
                 print(cell, end=' ')
             print()
-
-    def solve(self):
-        pass
 
 
 def main():
