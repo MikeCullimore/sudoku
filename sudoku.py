0d1e34b8b06e9e5d6e72f4d652a89d0a5cdd9d75
Mike Cullimore <michael.cullimore11@gmail.com>
2021-01-03 00:24:15 +0000

    Working (copied exactly from video).

--- sudoku.py
+++ sudoku.py
@@ -6,150 +6,80 @@ Sudoku solver based on Computerphile YouTube video.
 https://www.youtube.com/watch?v=G_UYXzGuqvM
 
 todo:
+Refactor as OO: sudoku_oo.py.
 Logging (to conveniently show/hide debug info.)
-Visualisation:
-    Not only current state but also options.
-    Web? (React? Elm? Vanilla JS?)
+Visualisation: web? (React? Elm? Vanilla JS?)
+Profile.
+Optimise.
+    Flat array not list of lists.
+    Avoid nested for loops.
+    Store range(9).
 """
 
-
-# Example from card.
-example = """
--36-----8
-8---423-1
-----3--9-
-----8-42-
-4-------5
--67-5----
--2--6----
-6-849---2
-1-----98-
-"""
-
-# Example from Dad.
-example2 = """
---7------
---9----54
-16---49--
--------93
-----694-7
---8-2----
---3-8-2--
--8-5----1
--1-97--6-
-"""
 
 # Example from video.
-example3 = """
-53--7----
-6--195---
--98----6-
-8---6---3
-4--8-3--1
-7---2---6
--6----28-
----419--5
-----8--79
-"""
+grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
 
 
-class Sudoku:
-    """
-    Sudoku puzzle.
+def pprint(grid):
+    for r in range(9):
+        for c in range(9):
+            value = grid[r][c]
+            if value == 0:
+                print(' ', end=' ')
+            else:
+                print(value, end=' ')
+        print()
 
-    Convention: row 0 is the top, column 0 is the left.
-    """
 
-    def solve(self):
-        for r in range(9):
-            for c in range(9):
-                if self._cells[r][c] == 0:
-                    for n in range(1, 10):
-                        if self.possible(c, r, n):
-                            self._cells[r][c] = n
-                            
-                            # debugging
-                            self.print()
-                            print()
-                            
-                            self.solve()
-
-                            # Backtracking: this possibility didn't work out, so undo it.
-                            self._cells[r][c] = 0
-                    return
-    
-    def possible(self, c, r, n):
-        """Is the number n possible at column c, row r?"""
-        # Column rule.
-        for i in range(9):
-            if self._cells[i][c] == n:
+def possible(y, x, n):
+    global grid  # todo: as arg?
+    for i in range(0, 9):
+        if grid[y][i] == n:
+            return False
+    for i in range(0, 9):
+        if grid[i][x] == n:
+            return False
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if grid[y0+i][x0+j] == n:
                 return False
-        
-        # Row rule.
-        for i in range(9):
-            if self._cells[r][i] == n:
-                return False
-        
-        # Box rule.
-        c0 = (c//3)*3
-        r0 = (r//3)*3
-        for i in range(3):
-            for j in range(3):
-                if self._cells[r0+i][c0+j] == n:
-                    return False
-
-        return True
-
-    def print(self):
-        for r in range(9):
-            for c in range(9):
-                value = self._cells[r][c]
-                if value == 0:
-                    print(' ', end=' ')
-                else:
-                    print(value, end=' ')
-            print()
-    
-    def __init__(self, s=example):
-        """
-        todo: input validation e.g. enforce 9 rows and columns.
-        """
-        self._cells = []
-        s = s.strip()
-        rows = s.split()
-        for row in rows:
-            r = []
-            for col, c in enumerate(row):
-                if c is '-':
-                    value = 0
-                else:
-                    try:
-                        value = int(c)
-                    except:
-                        raise ValueError(f'Invalid character at row {row}, column {col}: {c}.')
-                r.append(value)
-            self._cells.append(r)
+    return True
 
 
-def main():
-    # s = Sudoku(example)
-    # s = Sudoku(example2)
-    s = Sudoku(example3)
-    
-    # s.print()
-    # print(s._cells)
-     
-    s.solve()
-    # s.print()
-    
-    # Debugging.
-    # print(len(s._cells))
-    # print(s._cells[1].value)
-    # print(s.cell(2, 1).value)
-    # print(s.col(1))
-    # print(s.row(1))
-    # print(s.box(1))
+def solve():
+    global grid  # todo: as arg?
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
+                for n in range(1, 10):
+                    if possible(y, x, n):
+                        grid[y][x] = n
+                        
+                        # # debugging
+                        # pprint(grid)
+                        # print()
+                        
+                        solve()
+
+                        # Backtracking: this possibility didn't work out, so undo it.
+                        grid[y][x] = 0
+                return
+    pprint(grid)
+    # input('Search for other solutions?')
+    return  # Stop after finding one solution.
 
 
-if __name__ == '__main__':
-    main()
+solve()
---
+++ sudoku_oo.py
@@ -0,0 +1,154 @@
+"""
+sudoku.py
+
+Sudoku solver based on Computerphile YouTube video.
+
+https://www.youtube.com/watch?v=G_UYXzGuqvM
+
+todo:
+Apply changes from sudoku.py (return condition is wrong in solve).
+Logging (to conveniently show/hide debug info.)
+Visualisation: web? (React? Elm? Vanilla JS?)
+"""
+
+
+# Example from card.
+example = """
+-36-----8
+8---423-1
+----3--9-
+----8-42-
+4-------5
+-67-5----
+-2--6----
+6-849---2
+1-----98-
+"""
+
+# Example from Dad.
+example2 = """
+--7------
+--9----54
+16---49--
+-------93
+----694-7
+--8-2----
+--3-8-2--
+-8-5----1
+-1-97--6-
+"""
+
+# Example from video.
+example3 = """
+53--7----
+6--195---
+-98----6-
+8---6---3
+4--8-3--1
+7---2---6
+-6----28-
+---419--5
+----8--79
+"""
+
+
+class Sudoku:
+    """
+    Sudoku puzzle.
+
+    Convention: row 0 is the top, column 0 is the left.
+    """
+
+    def solve(self):
+        for r in range(9):
+            for c in range(9):
+                if self._cells[r][c] == 0:
+                    for n in range(1, 10):
+                        if self.possible(c, r, n):
+                            self._cells[r][c] = n
+                            
+                            # debugging
+                            self.print()
+                            print()
+                            
+                            self.solve()
+
+                            # Backtracking: this possibility didn't work out, so undo it.
+                            self._cells[r][c] = 0
+                    return
+    
+    def possible(self, c, r, n):
+        """Is the number n possible at column c, row r?"""
+        # Column rule.
+        for i in range(9):
+            if self._cells[i][c] == n:
+                return False
+        
+        # Row rule.
+        for i in range(9):
+            if self._cells[r][i] == n:
+                return False
+        
+        # Box rule.
+        c0 = (c//3)*3
+        r0 = (r//3)*3
+        for i in range(3):
+            for j in range(3):
+                if self._cells[r0+i][c0+j] == n:
+                    return False
+
+        return True
+
+    def print(self):
+        for r in range(9):
+            for c in range(9):
+                value = self._cells[r][c]
+                if value == 0:
+                    print(' ', end=' ')
+                else:
+                    print(value, end=' ')
+            print()
+    
+    def __init__(self, s=example):
+        """
+        todo: input validation e.g. enforce 9 rows and columns.
+        """
+        self._cells = []
+        s = s.strip()
+        rows = s.split()
+        for row in rows:
+            r = []
+            for col, c in enumerate(row):
+                if c is '-':
+                    value = 0
+                else:
+                    try:
+                        value = int(c)
+                    except:
+                        raise ValueError(f'Invalid character at row {row}, column {col}: {c}.')
+                r.append(value)
+            self._cells.append(r)
+
+
+def main():
+    # s = Sudoku(example)
+    # s = Sudoku(example2)
+    s = Sudoku(example3)
+    
+    # s.print()
+    # print(s._cells)
+     
+    s.solve()
+    # s.print()
+    
+    # Debugging.
+    # print(len(s._cells))
+    # print(s._cells[1].value)
+    # print(s.cell(2, 1).value)
+    # print(s.col(1))
+    # print(s.row(1))
+    # print(s.box(1))
+
+
+if __name__ == '__main__':
+    main()
